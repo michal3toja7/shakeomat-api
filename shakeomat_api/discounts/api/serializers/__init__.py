90c3d7435ce2fb60aaea4267ed2d59684e4d6c11
Michał Bruzdowski <michal3toja7@gmail.com>
2022-11-16 22:32:41 +0100

    Init views files

--- shakeomat_api/discounts/api/serializers/__init__.py
+++ shakeomat_api/discounts/api/serializers/__init__.py
@@ -1,9 +1,12 @@
-from shakeomat_api.discounts.api.serializers \
-    .discount_card_serializer import DiscountCardSerializer
-from shakeomat_api.discounts.api.serializers \
-    .discount_coupon_serializer import DiscountCouponSerializer
-from shakeomat_api.discounts.api.serializers \
-    .discount_status_serializer import DiscountStatusSerializer
+from shakeomat_api.discounts.api.serializers.discount_card import (
+    DiscountCardSerializer
+)
+from shakeomat_api.discounts.api.serializers.discount_coupon import (
+    DiscountCouponSerializer
+)
+from shakeomat_api.discounts.api.serializers.discount_status import (
+    DiscountStatusSerializer
+)
 
 __all__ = [
     DiscountCardSerializer,
