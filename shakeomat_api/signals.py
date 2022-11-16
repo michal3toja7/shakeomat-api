f18db2a85701c418eb37774334d0f000df0ef464
Michał Bruzdowski <michal3toja7@gmail.com>
2022-11-16 23:11:39 +0100

    Added signals on create DiscountCoupon

---
+++ shakeomat_api/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from shakeomat_api.discounts.models import DiscountCoupon
+from shakeomat_api.discounts.models import DiscountStatus
+
+
+@receiver(post_save, sender=DiscountCoupon)
+def create_status(sender, instance: DiscountCoupon, created, **kwargs):
+    """
+    Django Signals. The function is called when a DiscountCoupon object is
+    created. It creates a statis object related to the one-to-one
+    relationship with the DiscountCoupon.
+    """
+    if created:
+        DiscountStatus.objects.create(discount_coupon=instance)
