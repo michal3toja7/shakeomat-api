67979f30ce40c2b7ebab3b246b4e61610fb9f7ba
Michał Bruzdowski <michal3toja7@gmail.com>
2022-11-30 21:05:16 +0100

    Added str representation for user

--- shakeomat_api/users/models.py
+++ shakeomat_api/users/models.py
@@ -23,3 +23,8 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        return self.username
